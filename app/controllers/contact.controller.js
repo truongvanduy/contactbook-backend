4416d6c43cbfa333a82e45f29d3491a64a0982c7
Duy <truongvanduy09072002@gmail.com>
2024-02-21 17:50:46 +0700

    refactor

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,23 +1,25 @@
-exports.create = (req, res) => {
-  res.send({ message: 'create handler' });
-};
-exports.findAll = (req, res) => {
-  res.send({ message: 'findAll handler' });
-};
+export default contacts = {
+  create: (req, res) => {
+    res.send({ message: 'create handler' });
+  },
+  findAll: (req, res) => {
+    res.send({ message: 'findAll handler' });
+  },
 
-exports.findOne = (req, res) => {
-  res.send({ message: 'findOne handler' });
-};
-exports.update = (req, res) => {
-  res.send({ message: 'update handler' });
-};
-exports.delete = (req, res) => {
-  res.send({ message: 'delete handler' });
-};
-exports.deleteAll = (req, res) => {
-  res.send({ message: 'deleteAll handler' });
-};
+  findOne: (req, res) => {
+    res.send({ message: 'findOne handler' });
+  },
+  update: (req, res) => {
+    res.send({ message: 'update handler' });
+  },
+  delete: (req, res) => {
+    res.send({ message: 'delete handler' });
+  },
+  deleteAll: (req, res) => {
+    res.send({ message: 'deleteAll handler' });
+  },
 
-exports.findAllFavorite = (req, res) => {
-  res.send({ message: 'findAllFavorite handler' });
+  findAllFavorite: (req, res) => {
+    res.send({ message: 'findAllFavorite handler' });
+  },
 };
